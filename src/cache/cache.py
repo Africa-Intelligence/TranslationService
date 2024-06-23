ee10b3828f9b36250f6e2a2bd60f61a86bd0abbf
Jonathen Sundy <jonathensundy@gmail.com>
2024-06-23 19:17:28 +0200

    Use condition from function defition instead of hard coding it

--- src/cache/cache.py
+++ src/cache/cache.py
@@ -1,16 +1,17 @@
 import pandas as pd
 from src.data.dataset_loader import DatasetLoader
 
+
 class Cache(object):
     def __init__(self, cache_dataset_name):
         self.cache_dataset_name = cache_dataset_name
         self.cache_dataset = DatasetLoader(cache_dataset_name)
 
     def get(self, row: pd.DataFrame, field: str) -> str:
-        instruction =  row["instruction"]
-        match = self.cache_dataset.df.loc[self.cache_dataset.df['instruction'] == instruction, 'code']
-        if not match.empty:
-            return match.values[0]
+        instruction = row["instruction"]
+        condition = self.cache_dataset.df.loc[self.cache_dataset.df['instruction'] == instruction, field]
+        if not condition.empty:
+            return condition.values[0]
         else:
             return ""
 
