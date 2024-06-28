2e87f7a26517ba8fd94317e5c69a389c208c115b
chadbrouze <chadbrouze1@gmail.com>
2024-06-28 14:40:56 +0200

    GPU support

--- src/client/opus_client.py
+++ src/client/opus_client.py
@@ -1,4 +1,5 @@
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+import torch
 
 
 class OpusClient(object):
@@ -8,8 +9,12 @@ class OpusClient(object):
         self.tokenizer = AutoTokenizer.from_pretrained(model_name)
         self.model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
 
+        # Check if a GPU is available and move the model to the GPU
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.model.to(self.device)
+
     def translate(self, text) -> str:
-        encoded_input = self.tokenizer(text, return_tensors="pt", padding=True)
+        encoded_input = self.tokenizer(text, return_tensors="pt", padding=True).to(self.device)
         translated_tokens = self.model.generate(**encoded_input)
         translated_text = self.tokenizer.batch_decode(
             translated_tokens, skip_special_tokens=True
